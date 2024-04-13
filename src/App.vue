664e5d3a0ff6d5c3d45b0302fc6b87006bcd168d
FrancescoCesari97 <freedus97@gmail.com>
2024-04-13 11:06:46 +0200

    prpva stampa card in pagina

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,38 @@ import axios from 'axios';
   export default {
     data(){
       return {
-        title: 'Vite Boolfolio'
+        title: 'Vite Boolfolio',
+
+        projects: [],
       };
     },
 
     created(){
-      axios.get('http://127.0.0.1:8000/api/projects').then((response) => {});
+      axios.get('http://127.0.0.1:8000/api/projects').then((response) => {
+        this.projects = response.data.data
+
+      });
+
     }
   };
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <div class="container">
+    <h1>{{ title }}</h1>
+
+    
+    <div class="row">
+        <div v-for="project in projects" class="col-4 g-3">
+          <div class="card h-100">
+            <h3>{{ project.title }}</h3>
+
+            <p>{{ project.content }}</p>
+          </div>
+        </div>
+      
+    </div>
+  </div>
 </template>
 
 <style  lang="scss">
