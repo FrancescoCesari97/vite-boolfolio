83502995170be7d1d3699530039600a8a60a9a44
FrancescoCesari97 <freedus97@gmail.com>
2024-04-15 14:48:13 +0200

    creato ProjectCard e collegato tramite props a projectlist ed aggiunta computed per il testo troppo lungo

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,39 @@
+<script>
+
+export default { 
+
+  props: { project: Object}, 
+
+  computed: {
+    abstract(){
+        return this.project.content.length > 45 ? this.project.content.substr(0, 15) + '...' :  this.project.content;
+    }
+  }
+    
+};
+
+
+</script>
+
+<template>
+    <div class="col">
+
+        <div class="card h-100">
+            <img v-if="project.image" :src="project.image" class="card-img-top" alt="">
+            <div class="card-body">
+                
+                <h3 class="card-title">{{ project.title }}</h3>
+                
+                <p class="card-text"> {{ abstract }}</p>
+                <a href="" class="btn btn-primary">dettaglio</a>
+            </div>
+        </div>
+        
+    </div>
+
+</template>
+
+<style  lang="scss">
+
+</style>
+
--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,6 +1,8 @@
 <script>
 import axios from 'axios';
 import {store} from '../store';
+
+import ProjectCard from './ProjectCard.vue'
 export default { 
 
     data(){
@@ -10,6 +12,8 @@ export default {
 
       };
     },
+
+    components: { ProjectCard },
 
     created(){
       axios.get(store.api.baseUrl + 'projects').then((response) => {
@@ -26,21 +30,12 @@ export default {
 
 <template>
 
-    <div class="row">
-        <div v-for="project in store.projects" class="col-4 g-3">
-
-            <div class="card h-100">
-                <img v-if="project.image" :src="project.image" class="card-img-top" alt="">
-                <div class="card-body">
-
-                    <h3 class="card-title">{{ project.title }}</h3>
     
-                    <p class="card-text"> {{ project.content }}</p>
-                    <a href="" class="btn btn-primary">dettaglio</a>
-                </div>
-            </div>
+        <div  class="row row-col-4 g-3">
+
+            <project-card v-for="project in store.projects" :project="project"/>
         </div>
-    </div>
+
 
 </template>
 
