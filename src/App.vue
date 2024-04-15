e56a6a71b5b760a1045cddfffc94ba350540428d
FrancescoCesari97 <freedus97@gmail.com>
2024-04-15 12:09:05 +0200

    creata l'appHeader ed importata nell'app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 import {store} from './store';
 
+import AppHeader from './components/AppHeader.vue';
+
   export default {
     data(){
       return {
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {};
+
+
+</script>
+
+<template>
+    <nav class="navbar bg-body-tertiary">
+  <form class="container-fluid">
+    <div class="input-group">
+      <span class="input-group-text" id="basic-addon1">@</span>
+      <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+    </div>
+  </form>
+</nav>
+</template>
+
+<style  lang="scss">
+  @import'/src/scss/general.scss';
+</style>
+
