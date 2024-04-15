2438d1ce69b2d3001f4598a8093d39025aac1306
FrancescoCesari97 <freedus97@gmail.com>
2024-04-15 16:56:45 +0200

    Aggiunto il metodo fetchProjects per poter essere utilizzato nella paginazione per raggiugere le altre pagine dei projects

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,45 +1,57 @@
 <script>
-import axios from 'axios';
-import {store} from '../store';
+import { store } from "../store";
+import axios from "axios";
 
-import ProjectCard from './ProjectCard.vue'
-export default { 
+import ProjectCard from "./ProjectCard.vue";
+export default {
+  data() {
+    return {
+      store,
 
-    data(){
-      return {
+      pagination: [],
+    };
+  },
 
-        store,
+  methods: {
+    fetchProjects(endpoint = store.api.baseUrl + 'projects') {
+      axios.get(endpoint).then((response) => {
+        store.projects = response.data.data;
 
-      };
+        this.pagination = response.data.links;
+    });
     },
+  },
 
-    components: { ProjectCard },
+  components: { ProjectCard },
 
-    created(){
-      axios.get(store.api.baseUrl + 'projects').then((response) => {
-        store.projects = response.data.data
-
-      });
-
-    }
-    
+  created() {
+    this.fetchProjects();
+  },
 };
-
-
 </script>
 
 <template>
+  <div class="row row-col-4 g-3">
+    <project-card v-for="project in store.projects" :project="project" />
+  </div>
 
-    
-        <div  class="row row-col-4 g-3">
-
-            <project-card v-for="project in store.projects" :project="project"/>
-        </div>
-
-
+  <nav aria-label="Page navigation example ">
+    <ul class="pagination my-3">
+      <li class="page-item" >
+        
+      </li>
+      <li class="page-item" @click="fetchProjects(link.url)"
+       :class="{
+        active: link.active,
+        disabled: !link.url,
+       }
+        "v-for='link in pagination'>
+       <a class="page-link" href="#" v-html="link.label"></a>
+       </li>
+     
+      <li class="page-item"></li>
+    </ul>
+  </nav>
 </template>
 
-<style  lang="scss">
-
-</style>
-
+<style lang="scss"></style>
