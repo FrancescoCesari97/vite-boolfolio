4c356312fe4d297126854b76f9ed258cd8cde792
FrancescoCesari97 <freedus97@gmail.com>
2024-04-15 15:50:51 +0200

    stampati anche le technology nelle card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -21,10 +21,20 @@ export default {
         <div class="card h-100">
             <img v-if="project.image" :src="project.image" class="card-img-top" alt="">
             <div class="card-body">
+
                 
                 <h3 class="card-title">{{ project.title }}</h3>
+
+                <span class="badge" :style="'background-color:' + project.type.color"> {{ project.type.label }} </span>
                 
                 <p class="card-text"> {{ abstract }}</p>
+
+                <div class="my-2">
+
+                    <span class="badge me-2" :style="'background-color:' + technology.color" v-for="technology in project.technology">{{ technology.label }}</span>
+                </div>
+
+
                 <a href="" class="btn btn-primary">dettaglio</a>
             </div>
         </div>
