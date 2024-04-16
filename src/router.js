14acf21ba89060b20025ea3ac0a1544dc2c1bd5a
FrancescoCesari97 <freedus97@gmail.com>
2024-04-16 11:02:47 +0200

    creata la const router poi esportata nel file main.js

--- src/router.js
+++ src/router.js
@@ -3,3 +3,22 @@ import { createRouter, createWebHIstory } from "vue-router";
 import HomePage from " /pages/HomePge.vue";
 
 import ProjectPage from " /pages/ProjectPage.vue";
+
+const router = createRouter({
+  history: createWebHIstory(),
+
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      components: HomePage,
+    },
+    {
+      path: "/project",
+      name: "project",
+      components: ProjectPage,
+    },
+  ],
+});
+
+export { router };
